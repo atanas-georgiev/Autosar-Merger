fe15d3f30a05758a5884f4af5e376882e15abce0
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-10-28 12:11:03 +0200

    Separated diff and merge in different files.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,240 +10,36 @@ namespace ConsoleApplication1
     using System.Xml.Linq;
     using System.Xml.XPath;
 
+    using ConsoleApplication1.Models;
+    using ConsoleApplication1.XmlCompare;
+
     using XmlDiff;
 
 
 
     static class Program
     {
-        private static List<DiffData> data;
+        private static List<DiffDataElement> data;
 
         private static XDocument doc1;
 
         private static XDocument doc2;
 
-        static void GetElements(DiffNode element)
+
+        private static void Main(string[] args)
         {
-            var elements = element.Childs;
+            // compare
+            DiffDataGenerator gen = new DiffDataGenerator(@"D:\old.xml", @"D:\new.xml", "AUTOSAR.xsd");
+            var res = gen.GenerateDiffData();
+            DataStore.Save(res, "data.res");
 
-            foreach (var e in elements)
-            {
-                if (e.DiffAction != null)
-                {
-                    DiffData data1 = new DiffData();
-                    data1.Element = e.Raw;
-                    data1.Action = e.DiffAction.ToString();
-                    data1.FullXPath = e.Raw.AbsoluteXPath();                    
-                    data.Add(data1);
-                }
-                else if (e.IsChanged && e.Childs.ToList().Count == 0)
-                {
-                    DiffData data1 = new DiffData();
-                    data1.ChangedElement = e.Raw;
-                    data1.Action = "Changed";
-                    data1.FullXPath = e.Raw.AbsoluteXPath();                                        
-                    data1.Element = doc1.XPathSelectElement(data1.FullXPath);
-                    if (data1.Element == null)
-                    {
-                        throw new Exception();
-                    }
-                    data.Add(data1);
-                }
+            // merge
 
-                GetElements(e);                
-            }
+            var result = DataStore.Load<List<DiffDataElement>>("data.res");            
+            DiffDataReader reader = new DiffDataReader(result);
+            reader.ApplyDifferencesToFile(@"d:\old1.xml");
+
         }
 
-        public static void Sort(this XElement source, bool bSortAttributes = true)
-        {
-            //Make sure there is a valid source
-            if (source == null) throw new ArgumentNullException("source");
-
-            //Sort attributes if needed
-            if (bSortAttributes)
-            {
-                List<XAttribute> sortedAttributes = source.Attributes().OrderBy(a => a.ToString()).ToList();
-                sortedAttributes.ForEach(a => a.Remove());
-                sortedAttributes.ForEach(a => source.Add(a));
-            }
-
-            //Sort the children IF any exist
-            List<XElement> sortedChildren = source.Elements().OrderBy(e => e.Name.ToString()).ToList();
-            if (source.HasElements)
-            {
-                source.RemoveNodes();
-                sortedChildren.ForEach(c => c.Sort());
-                sortedChildren.ForEach(c => source.Add(c));
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            data = new List<DiffData>();
-            var comparer = new XmlComparer();
-
-            doc1 = XDocument.Load(@"D:\new.xml");
-            doc2 = XDocument.Load(@"D:\old.xml");
-            //doc1.Root.Sort();
-            //doc2.Root.Sort();
-
-            var diff = comparer.Compare(doc1.Root, doc2.Root);
-            GetElements(diff);
-            
-
-            //var d = data.g(x => x.FullXPath).ToList();
-
-
-            //for (int i = 0; i < data.Count; i++)
-            //{
-            //    for (int j = 0; j < data.Count; j++)
-            //    {
-            //        if (XNode.DeepEquals(data[i].Element, data[j].ChangedElement)
-            //            && XNode.DeepEquals(data[j].Element, data[i].ChangedElement))
-            //        {
-            //            data[i].ToBeDeleted = string.Empty;
-            //            data[j].ToBeDeleted = string.Empty;
-            //        }
-            //    }
-            //}
-
-            //data.RemoveAll(x => x.ToBeDeleted == string.Empty);
-
-            DataStore.Save(data, "data.res");
-
-            // second part 
-
-            var result = DataStore.Load<List<DiffData>>("data.res");
-            var elementsToDelete = new List<XElement>();
-
-            foreach (DiffData r in result)
-            {
-                try
-                {
-                    r.ChangedElement.Attribute("xmlns").Remove();
-                }
-                catch (Exception)
-                {
-
-                    // nothing
-                }
-
-                try
-                {
-                    r.Element.Attribute("xmlns").Remove();
-                }
-                catch (Exception)
-                {
-
-                    // nothing
-                }
-
-                switch (r.Action)
-                {
-                    case "Changed":
-                        var node = doc1.XPathSelectElement(r.FullXPath);
-
-                        if (node == null)
-                        {
-                            throw new Exception();
-                        }
-
-                        //XAttribute attr = r.ChangedElement.Attribute("xmlns");
-                        //try
-                        //{
-                        //    r.ChangedElement.Attribute("xmlns").Remove();
-                        //}
-                        //catch (Exception)
-                        //{
-                            
-                        //    // nothing
-                        //}
-                        
-                        node.ReplaceWith(new XElement(r.ChangedElement));
-                      
-                        break;
-                    case "Removed":
-                        var node2 = doc1.XPathSelectElement(r.FullXPath);
-                        try
-                        {
-                            elementsToDelete.Add(node2);
-                         //   node2.Remove();
-                        }
-                        catch (Exception)
-                        {
-
-                            var a = 1;
-                        }
-                        
-
-                        break;
-                    case "Added":
-                        var path = ConvertAddPath(r.FullXPath);
-                        var node3 = doc1.XPathSelectElement(path.Item1);
-
-                        if (path.Item2 == 0)
-                        {
-                            node3.Add(new XElement(r.Element));
-                        }
-                        else
-                        {
-                            var childrens = node3.Elements().ToArray();
-                            var child = childrens[path.Item2 - 1];
-
-                            child.AddAfterSelf(new XElement(r.Element));
-                        }
-
-                        break;
-
-                }
-            }
-
-
-            foreach (var del in elementsToDelete)
-            {
-                del.Remove();
-            }
-
-
-            doc1.Save("result.xml");
-        }
-
-        private static Tuple<string, int> ConvertAddPath(string path)
-        {
-            var fullPath = path.Split('/');
-            var sb = new StringBuilder();
-
-            for (int i = 0; i < fullPath.Length - 1; i++)
-            {
-                if (fullPath[i] != "")
-                {
-                    sb.Append("/");
-                    sb.Append(fullPath[i]);
-                }                
-            }
-
-            var fullIndexes = path.Split(new char[] { '[', ']' });
-            var indexToAdd = int.Parse(fullIndexes[fullIndexes.Length - 2]);
-
-            if (indexToAdd > 0)
-            {
-                indexToAdd -= 1;
-            }
-
-            var result = new Tuple<string, int>(sb.ToString(), indexToAdd);
-
-            return result;
-        }
-
-        public static XElement RemoveAllNamespaces(XElement e)
-        {
-            return new XElement(e.Name.LocalName,
-              (from n in e.Nodes()
-               select ((n is XElement) ? RemoveAllNamespaces(n as XElement) : n)),
-                  (e.HasAttributes) ?
-                    (from a in e.Attributes()
-                     where (!a.IsNamespaceDeclaration)
-                     select new XAttribute(a.Name.LocalName, a.Value)) : null);
-        }          
     }
 }
---
+++ ConsoleApplication1/XmlCompare/DiffDataGenerator.cs
@@ -0,0 +1,130 @@
+﻿namespace ConsoleApplication1.XmlCompare
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+    using System.Xml.Schema;
+    using System.Xml.XPath;
+
+    using ConsoleApplication1.Models;
+
+    using XmlDiff;
+
+    class DiffDataGenerator
+    {
+        private string xsdSchema;
+
+        private XDocument firstDoc;
+
+        private XDocument secondDoc;   
+
+        private List<DiffDataElement> diffData;
+
+        public DiffDataGenerator(string xmlFile1, string xmlFile2, string xsdSchema)
+        {            
+            // validate input files
+            this.firstDoc = XDocument.Load(xmlFile1);
+            this.secondDoc = XDocument.Load(xmlFile2);
+
+            if (this.firstDoc == null || this.secondDoc == null)
+            {
+                throw new ArgumentNullException("Input files are not valid xml documents!");
+            }
+
+            this.xsdSchema = xsdSchema;
+
+            // validate input documents
+            this.XsdSchemaValidation();            
+        }
+
+        public List<DiffDataElement> GenerateDiffData()
+        {
+            var comparer = new XmlComparer();
+            var diffNode = comparer.Compare(this.firstDoc.Root, this.secondDoc.Root);
+            this.diffData = new List<DiffDataElement>();
+            this.GetElements(diffNode);
+            return this.diffData;
+        }
+
+        private void XsdSchemaValidation()
+        {
+            var ns = this.firstDoc.Root.GetDefaultNamespace();
+            var ns2 = this.secondDoc.Root.GetDefaultNamespace();
+
+            if (ns.NamespaceName != ns2.NamespaceName)
+            {
+                throw new XmlSchemaException("Input files do not have the same namespaces!");
+            }
+
+            var schemas = new XmlSchemaSet();
+            schemas.Add(ns.NamespaceName, this.xsdSchema);
+
+            this.firstDoc.Validate(schemas, this.ValidationEventHandler);
+            this.secondDoc.Validate(schemas, this.ValidationEventHandler);            
+        }
+
+        private void ValidationEventHandler(object sender, ValidationEventArgs validationEventArgs)
+        {
+            Console.WriteLine("Done!");            
+        }
+
+        private void GetElements(DiffNode element)
+        {
+            var elements = element.Childs;
+
+            foreach (var e in elements)
+            {
+                if (e.DiffAction != null)
+                {
+                    var data1 = new DiffDataElement
+                                    {
+                                        Element = e.Raw,
+                                        Action = e.DiffAction.ToString(),
+                                        FullXPath = e.Raw.AbsoluteXPath()
+                                    };
+                    try
+                    {
+                        data1.Element.Attribute("xmlns").Remove();
+                    }
+                    catch (Exception)
+                    {
+
+                        // nothing
+                    }
+
+                    this.diffData.Add(data1);
+                }
+                else if (e.IsChanged && e.Childs.ToList().Count == 0)
+                {
+                    var data1 = new DiffDataElement
+                                    {
+                                        ChangedElement = e.Raw,
+                                        Action = "Changed",
+                                        FullXPath = e.Raw.AbsoluteXPath()
+                                    };
+                    data1.Element = this.firstDoc.XPathSelectElement(data1.FullXPath);
+                    
+                    if (data1.Element == null)
+                    {
+                        throw new Exception();
+                    }
+
+                    try
+                    {
+                        data1.ChangedElement.Attribute("xmlns").Remove();
+                    }
+                    catch (Exception)
+                    {
+
+                        // nothing
+                    }
+
+                    this.diffData.Add(data1);
+                }
+
+                this.GetElements(e);
+            }
+        }
+    }
+}
